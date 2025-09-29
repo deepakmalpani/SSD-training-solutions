0ca3047751fe6e0dc3c3aea14c1765ecd9a616f2
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-29 22:04:44 +0530

    Using closures instead since partial function is language specific

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,11 @@
 from functools import partial
 
-def char_startswith(input_str: str, char: str):
-    if len(input_str) > 0 and input_str.startswith(char):
-        return True
-    return False
+def char_startswith(char: str):
+    def word_startswith(word: str):
+        if len(word) > 0 and word.startswith(char):
+            return True
+        return False
+    return word_startswith
 
 def extract_strings_from_list(check_function, input_list: list):
     res = []
@@ -16,5 +18,5 @@ def extract_strings_from_list(check_function, input_list: list):
 if __name__ == "__main__":
     input_list = ["Exxon","Mobil","Bangalore","Bdd","Encapsulation"]
     start_char = "E"
-    check_function = partial(char_startswith, char = start_char)
+    check_function = char_startswith(start_char)
     print(extract_strings_from_list(check_function, input_list))
