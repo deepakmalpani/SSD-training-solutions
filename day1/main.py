b217d7302dee62b32d73d841c3e28709c8f61809
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-29 21:19:25 +0530

    Refactor

--- day1/main.py
+++ day1/main.py
@@ -1,15 +1,14 @@
 from functools import partial
 
-
 def char_exists_at_position(input_str: str, char: str, position: int):
     if len(input_str) > position and input_str[position] == char:
         return True
     return False
 
-def extract_strings_from_list(check_function, input_list: list, char: str):
+def extract_strings_from_list(check_function, input_list: list):
     res = []
     for word in input_list:
-        if check_function(word, char):
+        if check_function(word):
             res.append(word)         
     return res
 
@@ -18,5 +17,5 @@ if __name__ == "__main__":
     input_list = ["Exxon","Mobil","Bangalore","Bdd","Encapsulation"]
     start_char = "E"
     position = 0
-    check_function = partial(char_exists_at_position, position = position)
-    print(extract_strings_from_list(check_function, input_list, start_char))
+    check_function = partial(char_exists_at_position, position = position, char = start_char)
+    print(extract_strings_from_list(check_function, input_list))
