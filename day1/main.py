046861aad9bf2f1530f4efd06e42035573e39f1f
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-29 21:26:06 +0530

    Refactor

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,7 @@
 from functools import partial
 
-def char_exists_at_position(input_str: str, char: str, position: int):
-    if len(input_str) > position and input_str[position] == char:
+def char_startswith(input_str: str, char: str):
+    if len(input_str) > 0 and input_str.startswith(char):
         return True
     return False
 
@@ -16,6 +16,5 @@ def extract_strings_from_list(check_function, input_list: list):
 if __name__ == "__main__":
     input_list = ["Exxon","Mobil","Bangalore","Bdd","Encapsulation"]
     start_char = "E"
-    position = 0
-    check_function = partial(char_exists_at_position, position = position, char = start_char)
+    check_function = partial(char_startswith, char = start_char)
     print(extract_strings_from_list(check_function, input_list))
