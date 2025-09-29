d1c88af9addd15316575e0e7d8ae504b3ee92811
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-29 20:56:46 +0530

    Refactor code

--- day1/main.py
+++ day1/main.py
@@ -1,8 +1,13 @@
-def extract_strings_from_list(arr: list, start_char: str):
+def char_exists_at_position(input_str: str, char: str, position: int):
+    if len(input_str) > position and input_str[position] == char:
+        return True
+    return False
+
+def extract_strings_from_list(input_list: list, char: str, position: int):
     res = []
-    for a in arr:
-        if a[:1] == start_char:
-            res.append(a)
+    for word in input_list:
+        if char_exists_at_position(word, char, position):
+            res.append(word)
             
     return res
 
@@ -10,4 +15,5 @@ def extract_strings_from_list(arr: list, start_char: str):
 if __name__ == "__main__":
     input_list = ["Exxon","Mobil","Bangalore","Bdd","Encapsulation"]
     start_char = "E"
-    print(extract_strings_from_list(input_list, start_char))
+    position = 0
+    print(extract_strings_from_list(input_list, start_char, position))
